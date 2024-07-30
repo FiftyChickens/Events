2f81d614a7ca67a52bd3645e31522728d7431f65
Bryan Vu <bryanvu@me.com>
2024-07-30 16:07:40 -0500

    2. Requested values are stored

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const boxContainer = document.querySelector("#box-container");
+  const boxButton = document.querySelector("#new-box-button");
+  const colorForm = document.getElementById("color-form");
+  const inputColor = document.querySelector("#color-input");
 
 });
